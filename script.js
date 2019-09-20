09b67c6ac06998b559df43f53588c5fd43ccd88e
Dominik Buszowiecki <DominikB2014@gmail.com>
2019-09-20 17:06:56 -0400

    Made the navBar update by page.

--- script.js
+++ script.js
@@ -1,16 +1,29 @@
-const nav = document.getElementById("navBar") || null;
+const nav = document.getElementsByClassName("navBar");
 const pages = document.getElementsByClassName("page") || null;
 let page = 0;
 
+window.onload = function(){updateNav()};
 window.onscroll = function () {updateNav()};
 
 function updateNav() {
-    let height = pages[1].getBoundingClientRect().top - pages[0].getBoundingClientRect().top;
-    let pos = window.pageYOffset - 8;
-    // Loop determines which page the viewer is on
-    for (let i = 0; i < pages.length; i++){
-        if (pos > height*i - height*0.5){page = i}
-    }
+    updatePageNum(window.pageYOffset - 8);
+    setNav(page);
     console.log(page);
+
+}
+
+function updatePageNum(currentPosition){
+    let height = pages[1].getBoundingClientRect().top - pages[0].getBoundingClientRect().top;
+    let pageFactor = 0.3; //How much of the next page must be hidden
+    for (let i = 0; i < pages.length; i++){
+        if (currentPosition > height*i - height*pageFactor){page = i}
+    }
+}
+
+function setNav(pageNumber){
+    for (let i = 0; i < pages.length; i++){
+        if ((pageNumber - 1) === i){nav[i].classList.add("navBar-current")}
+        else {nav[i].classList.remove("navBar-current")}
+    }
 }
 
