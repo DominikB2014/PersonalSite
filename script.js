aaa6c5497aed1b08abbb36af69130d09bdf8a848
Dominik Buszowiecki <DominikB2014@gmail.com>
2019-09-20 17:42:47 -0400

    Fixed error where hover effect still worked on current navBar page.

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const nav = document.getElementsByClassName("navBar");
+const navLinks = document.getElementsByTagName("a");
 const pages = document.getElementsByClassName("page") || null;
 let page = 0;
 
@@ -8,12 +9,12 @@ window.onscroll = function () {updateNav()};
 function updateNav() {
     updatePageNum(window.pageYOffset - 8);
     setNav(page);
-    console.log(page);
-
 }
 
 function updatePageNum(currentPosition){
-    let height = pages[1].getBoundingClientRect().top - pages[0].getBoundingClientRect().top;
+    let height = pages[0].clientHeight;
+    console.log(height);
+    console.log(currentPosition);
     let pageFactor = 0.3; //How much of the next page must be hidden
     for (let i = 0; i < pages.length; i++){
         if (currentPosition > height*i - height*pageFactor){page = i}
@@ -21,9 +22,14 @@ function updatePageNum(currentPosition){
 }
 
 function setNav(pageNumber){
-    for (let i = 0; i < pages.length; i++){
-        if ((pageNumber - 1) === i){nav[i].classList.add("navBar-current")}
-        else {nav[i].classList.remove("navBar-current")}
+    for (let i = 0; i < pages.length - 1; i++){
+        if ((pageNumber - 1) === i){
+            nav[i].classList.add("navBar-current");
+            navLinks[i].classList.add("navBar-current")}
+        else {
+            nav[i].classList.remove("navBar-current");
+            navLinks[i].classList.remove("navBar-current");
+        }
     }
 }
 
