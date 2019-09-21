aef75825aec55c0915e4890ff52b45136698c445
Dominik Buszowiecki <DominikB2014@gmail.com>
2019-09-20 21:10:09 -0400

    Added comments to functions in JS

--- js/script.js
+++ js/script.js
@@ -1,16 +1,40 @@
 const nav = document.getElementsByClassName("navBar");
 const navLinks = document.getElementsByTagName("a");
 const pages = document.getElementsByClassName("page") || null;
-let page = 0;
+
+let page = 0; //Current Page Number
 
 window.onload = function(){updateNav()};
 window.onscroll = function () {updateNav()};
 
+
+/**
+Updates the nav bar by highlighting current section of the page
+ */
 function updateNav() {
     updatePageNum(window.pageYOffset - 8);
     setNav(page);
 }
 
+/**
+Smooth Scrolling Effect
+ */
+$(document).ready(function () {
+    let scrollTo = $(".scroll");
+    scrollTo.click(function (e) {
+        e.preventDefault();
+        $('body,html').animate({
+            scrollTop:$(this.hash).offset().top
+        })
+    })
+});
+
+/* --------- Helper Functions --------- */
+
+/**
+ * Updates the global page number variable
+ * @param currentPosition - scroll position (distance from top)
+ */
 function updatePageNum(currentPosition){
     let height = pages[0].clientHeight;
     let pageFactor = 0.3; //How much of the next page must be hidden
@@ -18,6 +42,10 @@ function updatePageNum(currentPosition){
     updateTitle(page);
 }
 
+/**
+ * Updates the document title (tab text)
+ * @param page - the current page number
+ */
 function updateTitle(page){
     switch(page){
         case 0:{document.title = "Home"; break;}
@@ -28,6 +56,10 @@ function updateTitle(page){
     }
 }
 
+/**
+ * Highlights the selected page on the navbar based on the page number
+ * @param pageNumber - what page the user is on
+ */
 function setNav(pageNumber){
     for (let i = 0; i < pages.length - 1; i++){
         if ((pageNumber - 1) === i){
@@ -39,13 +71,3 @@ function setNav(pageNumber){
         }
     }
 }
-
-$(document).ready(function () {
-    let scrollTo = $(".scroll");
-    scrollTo.click(function (e) {
-        e.preventDefault();
-        $('body,html').animate({
-            scrollTop:$(this.hash).offset().top
-        })
-    })
-});
